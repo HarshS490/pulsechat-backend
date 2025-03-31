6f11192ec5c972100140a6e24f27807967bf6714
Harsh Singh <harsh.singh30112004@gmail.com>
2025-03-31 11:02:47 +0530

    resolved authentication issues in middleware

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -7,16 +7,13 @@ const authMiddleWare = (req:Request,res:Response,next:NextFunction)=>{
     return res.status(401).json({status:401,message:"Unauthorized"});
   }
   const token = authHeader.split(" ")[1];
-
-  //verify token
-  const auth_secret = process.env.AUTH_SECRET;
+  const auth_secret = process.env.JWT_SECRET;
   if(!auth_secret){
     throw new Error("Missing jwt secret from environment variables.");
   }
 
   jwt.verify(token,auth_secret,(err,user)=>{
     if(err) return res.status(401).json({status:401,message:"Unauthorized"});
-
     req.user = user as AuthUser;
   });
   next();
