c09c0c6492090468e3bcc214f844d02c7246b53e
Harsh Singh <harsh.singh30112004@gmail.com>
2025-03-31 10:57:57 +0530

    removed cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,29 @@ import "dotenv/config";
 import cors from "cors";
 import router from "./routes/routes.js";
 import { Server } from "socket.io";
-import {createServer} from "http";
+import { createServer } from "http";
 import { setupSocket } from "./socket.js";
 import { createAdapter } from "@socket.io/redis-streams-adapter";
 import redisClient from "./config/redis.config.js";
 
-
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
-
 const server = createServer(app);
-export const io = new Server(server,{
-  cors:{
-    origin:"*"
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
   },
   adapter: createAdapter(redisClient),
 });
 
 setupSocket(io);
 
-
-// * Middleware 
+// * Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use("/api",router);
+app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   return res.send("It's working");
