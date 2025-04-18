535e673efd7ab8457bef0d497e7ef9770ab3ed52
Harsh Singh <harsh.singh30112004@gmail.com>
2025-04-18 20:18:40 +0530

    associating routerhandlers with apicalls

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import AuthController from "../controllers/auth.controller.js";
 import ConversationController from "../controllers/conversation.controller.js";
 import { authMiddleWare } from "../middlewares/AuthMiddleware.js";
 import { UserController } from "../controllers/users.controller.js";
+import FriendController from "../controllers/friends.controller.js";
 
 const router = Router();
 
@@ -10,7 +11,15 @@ router.post("/auth/login", AuthController.login);
 
 router.post("/new/chat", authMiddleWare, ConversationController.create);
 router.get("/chats/all",authMiddleWare,ConversationController.getConversations);
+router.post("/chat/id",ConversationController.getConversationById);
+router.get("/chat/messages",authMiddleWare,ConversationController.getMessages);
+router.post("/chat/send/message",authMiddleWare,ConversationController.sendMessage);
 
-router.get("/users/all",UserController.getAllUsers);
+router.post("/friendrequest/send",authMiddleWare,FriendController.sendFriendRequest);
+router.get("/friend/requests",authMiddleWare,FriendController.getFriendRequests);
+router.post("/friend/accept",authMiddleWare,FriendController.acceptFriendRequests);
+router.post("/friend/decline",authMiddleWare,FriendController.acceptFriendRequests);
+
+router.get("/users/all",authMiddleWare,UserController.getAllUsers);
 router.get("/user",UserController.getUserById);
 export default router;
