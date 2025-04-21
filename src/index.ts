350098444283e0d304e0524a074033103e189ec2
Harsh Singh <harsh.singh30112004@gmail.com>
2025-04-21 08:02:26 +0530

    configuring CORS

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,9 @@ startMessageConsumer();
 const server = createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
+    methods:["GET","POST"],
+    credentials:true
   },
 });
 
